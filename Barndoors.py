521be8b8a60a88e1ed959b5c7cc9388d0735ef24
diekev <kevin.dietrich@mailoo.org>
2014-01-17 01:09:57 +0100

    add mesh from pydata

--- Barndoors.py
+++ Barndoors.py
@@ -2,36 +2,26 @@ import bpy
 
 #create the barndoors objects
 class AddBarndoors(bpy.types.Operator):
-    mesh = bpy.ops.mesh
-    context = bpy.context
-    ops = bpy.ops
-     
-    mesh.primitive_plane_add()
-    context.object.name = "Right_Panel"
-    context.object.parent = bpy.data.objects["Spot"] 
-    ops.transform.translate(value=(1.5, 0, 0))
-    ops.object.transform_apply(location=True)
+    
+    me = bpy.data.meshes.new("Barndoors")
+    ob = bpy.data.objects.new("Barndoors", me)
+    
+    bpy.data.scenes[0].objects.link(ob)
 
-    mesh.primitive_plane_add()
-    context.object.name = "Left_Panel"
-    context.object.parent = bpy.data.objects["Spot"] 
-    ops.transform.translate(value=(-1, 0, 0))
-    ops.object.transform_apply(location=True)
-    ops.transform.translate(value=(-1, 0, 0))
-
-    mesh.primitive_plane_add()
-    context.object.name = "Top_Panel"
-    context.object.parent = bpy.data.objects["Spot"] 
-    ops.transform.translate(value=(0, 1.5, 0))
-    ops.object.transform_apply(location=True)
-
-    mesh.primitive_plane_add()
-    context.object.name = "Bottom_Panel"
-    context.object.parent = bpy.data.objects["Spot"] 
-    ops.transform.translate(value=(0, -1.5, 0))
-    ops.object.transform_apply(location=True)
-
-    from_pydata([(-1.0, -1.0, 0.0), (1.0, -1.0, 0.0), (-1.0, 1.0, 0.0), (1.0, 1.0, 0.0), (-3.17727, 3.17981, -0.04091), (-4.49474, 0.00163, -0.07223), (-3.17925, -3.1775, -0.06124), (-0.0014, -4.4953, -0.01438), (3.17727, -3.17981, 0.04091), (4.49474, -0.00163, 0.07223), (3.17925, 3.1775, 0.06124), (0.0014, 4.4953, 0.01438), (-1.0, 0.0, 0.0), (0.0, -1.0, 0.0), (1.0, 0.0, 0.0), (0.0, 1.0, 0.0)],[(12, 0), (13, 1), (14, 3), (15, 2), (5, 4), (6, 5), (7, 6), (8, 7), (9, 8), (10, 9), (11, 10), (4, 11), (12, 2), (13, 0), (14, 1), (15, 3), (2, 4), (11, 15), (3, 10), (14, 9), (5, 12), (0, 6), (7, 13), (1, 8)],[(14, 9, 10, 3), (10, 11, 15, 3), (15, 11, 4, 2), (4, 5, 12, 2), (12, 5, 6, 0), (6, 7, 13, 0), (13, 7, 8, 1), (8, 9, 14, 1)])
+    me.from_pydata([(-1.0, -1.0, 0.0), (1.0, -1.0, 0.0), (-1.0, 1.0, 0.0), (1.0, 1.0, 0.0), 
+                 (-3.17727, 3.17981, -0.04091), (-4.49474, 0.00163, -0.07223), 
+                 (-3.17925, -3.1775, -0.06124), (-0.0014, -4.4953, -0.01438), 
+                 (3.17727, -3.17981, 0.04091), (4.49474, -0.00163, 0.07223), 
+                 (3.17925, 3.1775, 0.06124), (0.0014, 4.4953, 0.01438), 
+                 (-1.0, 0.0, 0.0), (0.0, -1.0, 0.0), (1.0, 0.0, 0.0), 
+                 (0.0, 1.0, 0.0)],[(12, 0), (13, 1), (14, 3), (15, 2), (5, 4), 
+                 (6, 5), (7, 6), (8, 7), (9, 8), (10, 9), (11, 10), (4, 11), (12, 2), 
+                 (13, 0), (14, 1), (15, 3), (2, 4), (11, 15), (3, 10), (14, 9), (5, 12), 
+                 (0, 6), (7, 13), (1, 8)],[(14, 9, 10, 3), (10, 11, 15, 3), (15, 11, 4, 2),
+                 (4, 5, 12, 2), (12, 5, 6, 0), (6, 7, 13, 0), (13, 7, 8, 1), (8, 9, 14, 1)])
+    me.update()
+    
+#    bpy.context.object.parent = bpy.data.objects["Spot"]
     
 class ObjectsCreationControls(bpy.types.PropertyGroup):
     
@@ -46,7 +36,8 @@ class ObjectsCreationControls(bpy.types.PropertyGroup):
         name="",
         description="Angle of the right panel",
         unit="ROTATION",
-        soft_min=-3.14159265, soft_max=3.14159265, step=10.00, default=0.00)                      
+        soft_min=-3.14159265, soft_max=3.14159265, step=10.00, default=0.00)  
+                            
 class BarnDoors(bpy.types.Panel):
     
     bl_idname = "panel.BarnDoors"
